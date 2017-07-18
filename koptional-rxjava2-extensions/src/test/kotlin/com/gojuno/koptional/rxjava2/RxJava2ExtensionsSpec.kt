c0c1f429b69df969e4b1f421c5357c0b9072c940
ihar <ihar@gojuno.com>
2017-07-18 15:42:57 +0300

    Add filterSome and filterNone extensions for Flowable, Maybe and Single.

--- koptional-rxjava2-extensions/src/test/kotlin/com/gojuno/koptional/rxjava2/RxJava2ExtensionsSpec.kt
+++ koptional-rxjava2-extensions/src/test/kotlin/com/gojuno/koptional/rxjava2/RxJava2ExtensionsSpec.kt
@@ -1,62 +1,339 @@
 package com.gojuno.koptional.rxjava2
 
 import com.gojuno.koptional.None
+import com.gojuno.koptional.Optional
 import com.gojuno.koptional.Some
+import io.reactivex.Flowable
+import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.observers.TestObserver
+import io.reactivex.subscribers.TestSubscriber
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.it
 
 class RxJava2ExtensionsSpec : Spek({
 
-    val stream by memoized {
-        Observable.just(Some("a"), None, Some("b"), Some("c"), None)
-    }
+    context("Observable") {
 
-    context("filterSome") {
+        val stream by memoized {
+            Observable.just(Some("a"), None, Some("b"), Some("c"), None)
+        }
 
-        val subscriber by memoized {
-            TestObserver<String>().apply {
-                stream
-                        .filterSome()
-                        .subscribe(this)
+        context("filterSome") {
+
+            val subscriber by memoized {
+                TestObserver<String>().apply {
+                    stream
+                            .filterSome()
+                            .subscribe(this)
+                }
+            }
+
+            it("passes only Some values") {
+                subscriber.assertValues("a", "b", "c")
+            }
+
+            it("completes stream") {
+                subscriber.assertComplete()
+            }
+
+            it("does not emit error") {
+                subscriber.assertNoErrors()
             }
         }
 
-        it("passes only Some values") {
-            subscriber.assertValues("a", "b", "c")
-        }
+        context("filterNone") {
 
-        it("completes stream") {
-            subscriber.assertComplete()
-        }
+            val subscriber by memoized {
+                TestObserver<Unit>().apply {
+                    stream
+                            .filterNone()
+                            .subscribe(this)
+                }
+            }
 
-        it("does not emit error") {
-            subscriber.assertNoErrors()
+            it("passes only None values as Unit") {
+                subscriber.assertValues(Unit, Unit)
+            }
+
+            it("completes stream") {
+                subscriber.assertComplete()
+            }
+
+            it("does not emit error") {
+                subscriber.assertNoErrors()
+            }
         }
     }
 
-    context("filterNone") {
+    context("Flowable") {
 
-        val subscriber by memoized {
-            TestObserver<Unit>().apply {
-                stream
-                        .filterNone()
-                        .subscribe(this)
+        val stream by memoized {
+            Flowable.just(Some("a"), None, Some("b"), Some("c"), None)
+        }
+
+        context("filterSome") {
+
+            val subscriber by memoized {
+                TestSubscriber<String>().apply {
+                    stream
+                            .filterSome()
+                            .subscribe(this)
+                }
+            }
+
+            it("passes only Some values") {
+                subscriber.assertValues("a", "b", "c")
+            }
+
+            it("completes stream") {
+                subscriber.assertComplete()
+            }
+
+            it("does not emit error") {
+                subscriber.assertNoErrors()
             }
         }
 
-        it("passes only None values as Unit") {
-            subscriber.assertValues(Unit, Unit)
+        context("filterNone") {
+
+            val subscriber by memoized {
+                TestSubscriber<Unit>().apply {
+                    stream
+                            .filterNone()
+                            .subscribe(this)
+                }
+            }
+
+            it("passes only None values as Unit") {
+                subscriber.assertValues(Unit, Unit)
+            }
+
+            it("completes stream") {
+                subscriber.assertComplete()
+            }
+
+            it("does not emit error") {
+                subscriber.assertNoErrors()
+            }
+        }
+    }
+
+    context("Maybe") {
+
+        context("Stream with Some") {
+            val stream by memoized {
+                Maybe.just(Some("a") as Optional<String>)
+            }
+
+            context("filterSome") {
+
+                val subscriber by memoized {
+                    TestObserver<String>().apply {
+                        stream
+                                .filterSome()
+                                .subscribe(this)
+                    }
+                }
+
+                it("passes only Some values") {
+                    subscriber.assertValues("a")
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+
+            context("filterNone") {
+
+                val subscriber by memoized {
+                    TestObserver<Unit>().apply {
+                        stream
+                                .filterNone()
+                                .subscribe(this)
+                    }
+                }
+
+                it("do not emit values") {
+                    subscriber.assertNoValues()
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+
         }
 
-        it("completes stream") {
-            subscriber.assertComplete()
+        context("Stream with None") {
+            val stream by memoized {
+                Maybe.just(None as Optional<String>)
+            }
+
+            context("filterSome") {
+
+                val subscriber by memoized {
+                    TestObserver<String>().apply {
+                        stream
+                                .filterSome()
+                                .subscribe(this)
+                    }
+                }
+
+                it("do not emit values") {
+                    subscriber.assertNoValues()
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+
+            context("filterNone") {
+
+                val subscriber by memoized {
+                    TestObserver<Unit>().apply {
+                        stream
+                                .filterNone()
+                                .subscribe(this)
+                    }
+                }
+
+                it("passes only None value as Unit") {
+                    subscriber.assertValue(Unit)
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+        }
+    }
+
+    context("Single") {
+
+        context("Stream with Some") {
+            val stream by memoized {
+                Single.just(Some("a") as Optional<String>)
+            }
+
+            context("filterSome") {
+
+                val subscriber by memoized {
+                    TestObserver<String>().apply {
+                        stream
+                                .filterSome()
+                                .subscribe(this)
+                    }
+                }
+
+                it("passes only Some values") {
+                    subscriber.assertValues("a")
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+
+            context("filterNone") {
+
+                val subscriber by memoized {
+                    TestObserver<Unit>().apply {
+                        stream
+                                .filterNone()
+                                .subscribe(this)
+                    }
+                }
+
+                it("do not emit values") {
+                    subscriber.assertNoValues()
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+
         }
 
-        it("does not emit error") {
-            subscriber.assertNoErrors()
+        context("Stream with None") {
+            val stream by memoized {
+                Single.just(None as Optional<String>)
+            }
+
+            context("filterSome") {
+
+                val subscriber by memoized {
+                    TestObserver<String>().apply {
+                        stream
+                                .filterSome()
+                                .subscribe(this)
+                    }
+                }
+
+                it("do not emit values") {
+                    subscriber.assertNoValues()
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
+
+            context("filterNone") {
+
+                val subscriber by memoized {
+                    TestObserver<Unit>().apply {
+                        stream
+                                .filterNone()
+                                .subscribe(this)
+                    }
+                }
+
+                it("passes only None value as Unit") {
+                    subscriber.assertValue(Unit)
+                }
+
+                it("completes stream") {
+                    subscriber.assertComplete()
+                }
+
+                it("does not emit error") {
+                    subscriber.assertNoErrors()
+                }
+            }
         }
     }
 })
