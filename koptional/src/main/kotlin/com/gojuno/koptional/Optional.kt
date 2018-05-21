7c0fc11b4a4c9d0a8f9650aa2a275a625fbd3ea9
Artem Zinnatullin <artem.zinnatullin@gmail.com>
2018-05-21 12:47:44 -0700

    toNullable() specialization.

--- koptional/src/main/kotlin/com/gojuno/koptional/Optional.kt
+++ koptional/src/main/kotlin/com/gojuno/koptional/Optional.kt
@@ -2,18 +2,20 @@ package com.gojuno.koptional
 
 sealed class Optional<out T : Any> {
 
-    fun toNullable(): T? = when (this) {
-        is Some -> value
-        is None -> null
-    }
+    /**
+     * Converts [Optional] to either its non-null value if it's [Some] or `null` if it's [None].
+     */
+    abstract fun toNullable(): T?
 }
 
 data class Some<out T : Any>(val value: T) : Optional<T>() {
     override fun toString() = "Some($value)"
+    override fun toNullable(): T = value
 }
 
 object None : Optional<Nothing>() {
     override fun toString() = "None"
+    override fun toNullable(): Nothing? = null
 }
 
 fun <T : Any> T?.toOptional(): Optional<T> = if (this == null) None else Some(this)
--- koptional/src/test/kotlin/com/gojuno/koptional/OptionalSpec.kt
+++ koptional/src/test/kotlin/com/gojuno/koptional/OptionalSpec.kt
@@ -12,7 +12,7 @@ class OptionalSpec : Spek({
 
         context("Some.toNullable") {
 
-            val result = Some("string").toNullable()
+            val result: String = Some("string").toNullable()
 
             it("converts it to value") {
                 assertThat(result).isEqualTo("string")
@@ -21,7 +21,7 @@ class OptionalSpec : Spek({
 
         context("None.toNullable") {
 
-            val result = None.toNullable()
+            val result: Nothing? = None.toNullable()
 
             it("converts it to null") {
                 assertThat(result as Any?).isNull()
@@ -80,7 +80,6 @@ class OptionalSpec : Spek({
             it("converts it to String") {
                 assertThat(result).isEqualTo("None")
             }
-
         }
     }
 })
